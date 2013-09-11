d31de235f3d310baa79662d159c57b72dc354d97
xuhf <xuhf@fenbi.com>
2013-09-11 22:24:43 +0800

    FIX: consider sampleRate ADD: bytesToStr

--- sampler.py
+++ sampler.py
@@ -22,12 +22,19 @@ class Sampler(object):
         self.dbSize = self.conn.dbsize()
         if not self.sampleSize:
             self.sampleSize = int(0.1 * self.dbSize)
+        if self.dbSize == 0:
+            self.sampleRate = 0.0
+        elif self.sampleSize >= self.dbSize:
+            self.sampleRate = 1.0
+        else:
+            self.sampleRate = 1.0*self.sampleSize/self.dbSize
 
     def info(self):
         print 'INFO host=%s' % self.host
         print 'INFO port=%d' % self.port
         print 'INFO dbSize=%d' % self.dbSize
         print 'INFO sampleSize=%d' % self.sampleSize
+        print 'INFO sampleRate=%.1f%%' % (self.sampleRate*100)
 
     def start(self):
         self._setup()
@@ -41,9 +48,31 @@ class Sampler(object):
             objInfo = self.conn.debug_object(key)
             objSize = objInfo['serializedlength']
             trie.insert(key, objSize)
+        
+        def bytesToStr(bytes):
+            t = []
+            i = 0
+            while i<4 and bytes>0:
+                t.append(bytes % 1024)
+                bytes = bytes / 1024
+                i = i + 1
+            r = ''
+            for i in range(len(t)-1, -1, -1):
+                r = r + str(t[i])
+                if i == 3:
+                    r = r + 'G`'
+                if i == 2:
+                    r = r + 'M`'
+                if i == 1:
+                    r = r + 'K`'
+                if i == 0:
+                    r = r + 'B'
+            return r
 
         def cbk(node):
-            print '%s %s\t%d' % ('>'*node.depth, node.prefix, node.bytes)
+            realBytes = int(node.bytes/self.sampleRate)
+            byteStr = bytesToStr(realBytes)
+            print '%s %s\t%s' % ('>'*node.depth, node.prefix, byteStr)
 
         trie.dfs(cbk)
 
